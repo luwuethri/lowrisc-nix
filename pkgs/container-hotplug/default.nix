86370fda74111142a3ae50cfb40176bf0e965513
David James McCorrie <james.mccorrie@lowrisc.org>
2026-07-27 18:02:18 +0100

    chore: bump the version of container hotplug
    Signed-off-by: David James McCorrie <james.mccorrie@lowrisc.org>

--- pkgs/container-hotplug/default.nix
+++ pkgs/container-hotplug/default.nix
@@ -14,10 +14,10 @@
 # We currently fetch from container-hotplug directly due to complexity in building bpf-linker.
 stdenv.mkDerivation rec {
   name = "container-hotplug";
-  version = "20251118-1";
+  version = "20260727-1";
   src = fetchurl {
     url = "https://github.com/lowRISC/container-hotplug/releases/download/${version}/container-hotplug";
-    hash = "sha256-ZNx72Jx7GBahEBMidNFR9r2Jj5Yq91RpdVr5pTWFNMs=";
+    hash = "sha256-WsGXzHqZ8k3EzgIYYElN3Hw+hpSCpm0m0/ft01JeO1Q=";
   };
 
   buildInputs = [
